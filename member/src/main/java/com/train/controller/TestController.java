76e8188150166a15188dd310866ee43d6f65eeb0
chuanyin <1213707943@qq.com>
2023-07-25 19:51:56 +0800

    增加公共模块common，用于置放公共的类比如工具类，拦截器，AOP，常量，枚举公共配置等,以及公共配置文件

--- member/src/main/java/com/train/controller/TestController.java
+++ member/src/main/java/com/train/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     @GetMapping("test")
-    public void test() {
-        System.out.println("Hallo World");
+    public String test() {
+        return "Hallo World";
     }
 }
